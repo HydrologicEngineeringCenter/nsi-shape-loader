fb5c58c59bc4581f9ae89b87dee03f1a0f2e4ef4
TMNT <trietpersonal@gmail.com>
2022-02-28 12:02:28 -0500

    store

---
+++ internal/store/store.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"log"
+
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/config"
+	"github.com/usace/goquery"
+)
+
+type PSStore struct {
+	DS goquery.DataStore
+}
+
+func NewStore(c config.Config) (*PSStore, error) {
+	dbconf := c.Rdbmsconfig()
+	ds, err := goquery.NewRdbmsDataStore(&dbconf)
+	if err != nil {
+		log.Printf("Unable to connect to database during startup: %s", err)
+	} else {
+		log.Printf("Connected as %s to database %s:%s/%s", c.Dbuser, c.Dbhost, c.Dbport, c.Dbname)
+	}
+
+	ss := PSStore{ds}
+	return &ss, nil
+}
