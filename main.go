a52e47394cead2cc867e0c5ec71ff8cf80512120
TMNT <trietpersonal@gmail.com>
2022-02-28 12:01:49 -0500

    args

--- main.go
+++ main.go
@@ -4,13 +4,60 @@ import (
 	"log"
 	"os"
 
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/core"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	var dbuser, dbpass, dbname, dbhost, dbport, file string
 	app := &cli.App{
-		Name:  "nsi-loader",
-		Usage: "upload nsi shapefile to postgis database",
+		Name:   "nsi-loader",
+		Usage:  "upload nsi shapefile to postgis database",
+		Action: core.Upload,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "dbuser",
+				Aliases:     []string{"u"},
+				Usage:       "database username",
+				Destination: &dbuser,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "dbpass",
+				Aliases:     []string{"p"},
+				Usage:       "database password",
+				Destination: &dbpass,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "dbname",
+				Aliases:     []string{"n"},
+				Usage:       "database name",
+				Destination: &dbname,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "dbhost",
+				Aliases:     []string{"t"},
+				Usage:       "database hostname",
+				Destination: &dbhost,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "dbport",
+				Aliases:     []string{"o"},
+				Usage:       "database access port",
+				Destination: &dbport,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "shapefile",
+				Aliases:     []string{"d"},
+				Usage:       "input shapefile",
+				Destination: &file,
+				Required:    true,
+			},
+		},
 	}
 
 	err := app.Run(os.Args)
