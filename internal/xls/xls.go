17e389683fdf602f4a0619d9f65d4efedf3824aa
TMNT <trietpersonal@gmail.com>
2022-03-17 11:43:42 -0400

    excelize factory

--- internal/xls/xls.go
+++ internal/xls/xls.go
@@ -5,7 +5,7 @@ import "github.com/xuri/excelize/v2"
 func NewXls(src string) (*excelize.File, error) {
 	file, err := excelize.OpenFile(src)
 	if err != nil {
-		return err
+		return &excelize.File{}, err
 	}
 	defer func() {
 		// Close the spreadsheet.
