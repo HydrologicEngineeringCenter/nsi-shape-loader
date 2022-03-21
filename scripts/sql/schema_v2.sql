91c1758e17f4300b2344b4010c698284ea6ba98c
TMNT <trietpersonal@gmail.com>
2022-03-21 11:57:18 -0400

    keys + unique

--- scripts/sql/schema_v2.sql
+++ scripts/sql/schema_v2.sql
@@ -3,24 +3,25 @@ create table field (
     name text not null,
     type text not null,
     description text,
-    is_domain boolean not null
+    is_domain boolean not null,
+    unique(name, type)
 );
 
 create table domain (
     id uuid not null default gen_random_uuid() primary key,
-    nsi_field_id uuid not null,
+    field_id uuid not null,
     domain_value text not null,
     data_type text not null,
-    constraint fk_domain_nsi_field
-        foreign key(nsi_field_id)
-            references nsi_field(id)
+    constraint fk_domain_field
+        foreign key(field_id)
+            references field(id)
 );
 
 create table schema_field (
     id uuid not null default,
-    nsi_field_id uuid not null,
-    constraint fk_schema_field_nsi_field
-        foreign key(nsi_field_id)
+    field_id uuid not null,
+    constraint fk_schema_field_field
+        foreign key(field_id)
             references field(id)
     constraint fk_schema_field_schema
         foreign key(id)
@@ -46,7 +47,10 @@ create table access (
     dataset_id uuid not null,
     access_group text not null,
     role text not null,
-    permission text not null
+    permission text not null,
+    constraint fk_access_dataset
+        foreign key(dataset_id)
+            references dataset(id)
 );
 
 create table dataset (
@@ -61,6 +65,9 @@ create table dataset (
     date_created date not null default current_date,
     created_by text not null,
     quality_id uuid not null,
+    constraint fk_dataset_nsi_schema
+        foreign key(nsi_schema_id)
+            references nsi_schema(id),
     constraint fk_dataset_quality
         foreign key(quality_id)
             references quality(id)
