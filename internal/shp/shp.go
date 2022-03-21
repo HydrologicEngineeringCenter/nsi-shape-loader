2fcbc0b2abaaf33533261d053afdaa033ec20b99
TMNT <trietpersonal@gmail.com>
2022-03-21 13:29:05 -0400

    extend shp-go - find unique values in field

--- internal/shp/shp.go
+++ internal/shp/shp.go
@@ -1,9 +1,35 @@
 package shp
 
-import "github.com/jonas-p/go-shp"
+import (
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/util"
+	"github.com/jonas-p/go-shp"
+)
 
 func NewShp(src string) (*shp.Reader, error) {
 	shpf, err := shp.Open(src)
 	defer shpf.Close()
 	return shpf, err
+}
+
+// UniqueValues determines all unique values from field
+func UniqueValues(shpF *shp.Reader, f shp.Field) []string {
+	var vals []string
+	var fIdx int
+	fields := shpF.Fields()
+
+	// Loop to find field index
+	for i, field := range fields {
+		if field.String() == f.String() {
+			fIdx = i
+			break
+		}
+	}
+	// Loop to find all unique values
+	for i := 0; i < shpF.AttributeCount(); i++ {
+		val := shpF.ReadAttribute(i, fIdx)
+		if !util.StrContains(vals, val) {
+			vals = append(vals, val)
+		}
+	}
+	return vals
 }
