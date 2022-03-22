6bfdbb0db9472c4335efe563f5aa32bc2af2104e
TMNT <trietpersonal@gmail.com>
2022-03-22 12:12:17 -0400

    structuring framework

--- internal/core/core.go
+++ internal/core/core.go
@@ -2,24 +2,49 @@ package core
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/config"
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/files"
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/model"
 	shape "github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/shp"
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/store"
+	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/types"
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/xls"
+	"github.com/google/uuid"
+	"github.com/jonas-p/go-shp"
 	"github.com/urfave/cli/v2"
 )
 
 func Core(c *cli.Context) error {
-	// 2 modes to core functionalities
 	//  pre - generate config xls from shp
 	//  upload - upload based on data and metadata from xls and shp
 	cfg, err := config.NewConfig(c)
+
+	////////////////////////////////////////
+	//  OWNER
+
+	// Pre mode generates the metadata xls required by Upload
 	if cfg.Mode == config.Pre {
 		err = PreUpload(cfg)
-	} else {
-		err = PreUpload(cfg)
 	}
+	// Upload mode uploads the dataset and associated metadata
+	if cfg.Mode == config.Upload {
+		err = Upload(cfg)
+	}
+
+	////////////////////////////////////////
+	//  ADMIN
+
+	// Access mode modifies group access to the dataset
+	if cfg.Mode == config.Access {
+		err = ModAccess(cfg)
+	}
+	// Quality mode add
+	if cfg.Mode == config.Quality {
+		err = AddQuality(cfg)
+	}
+
 	return err
 }
 
@@ -34,8 +59,7 @@ func PreUpload(cfg config.Config) error {
 		return err
 	}
 
-	// populate fields worksheet based on shp fields
-	xls, err := xls.NewXls(cpXlsDest)
+	xlsF, err := xls.NewXls(cpXlsDest)
 	if err != nil {
 		return err
 	}
@@ -48,175 +72,244 @@ func PreUpload(cfg config.Config) error {
 	for j, f := range fields {
 		loc = "B" + fmt.Sprint(j+2)
 		val = f.String()
-		err = xls.SetCellValue("fields", loc, val)
+		err = xlsF.SetCellValue("fields", loc, val)
 		if err != nil {
 			return err
 		}
 	}
-	xls.Save()
+	xlsF.Save()
 	return err
 }
 
 //// Upload populates metadata from the config xls and upload data from shp file
-//func Upload(cfg config.Config) error {
-//	st, err := store.NewStore(cfg)
-//	if err != nil {
-//		return err
-//	}
+func Upload(cfg config.Config) error {
+	st, err := store.NewStore(cfg)
+	if err != nil {
+		return err
+	}
 
-//	// fields := shpf.Fields()
+	// fields := shpf.Fields()
 
-//	// var insertTable = dq.TableDataSet{
-//	// 	Name:       cfg.Dbtablename,
-//	// 	Statements: map[string]string{},
-//	// 	Fields:     model.Point{},
-//	// }
+	// var insertTable = dq.TableDataSet{
+	// 	Name:       cfg.Dbtablename,
+	// 	Statements: map[string]string{},
+	// 	Fields:     model.Point{},
+	// }
 
-//	// batchSize := 20000
-//	// lastRecordIdx := shpf.AttributeCount() - 1
-//	// var records []model.Point
-//	// for shpf.Next() {
-//	// 	i, _ := shpf.Shape()
+	// batchSize := 20000
+	// lastRecordIdx := shpf.AttributeCount() - 1
+	// var records []model.Point
+	// for shpf.Next() {
+	// 	i, _ := shpf.Shape()
 
-//	// 	// construct data struct from point
-//	// 	var newPoint model.Point
-//	// 	for j, f := range fields {
-//	// 		val := shpf.ReadAttribute(i, j)
-//	// 		fieldStr := strings.Title(strings.ToLower(f.String()))
-//	// 		structutil.SetField(&newPoint, fieldStr, val)
-//	// 	}
-//	// 	records = append(records, newPoint)
-//	// 	// Batch upload on reaching batchsize limit
-//	// 	if (i != 0 && i%batchSize == 0) || i == lastRecordIdx {
-//	// 		if i == lastRecordIdx {
-//	// 			// batching the last odd lot records doesn't work for some reason
-//	// 			err = st.DS.Insert(&insertTable).
-//	// 				Records(&records).
-//	// 				Execute()
-//	// 		} else {
-//	// 			err = st.DS.Insert(&insertTable).
-//	// 				Records(&records).
-//	// 				Batch(true).
-//	// 				BatchSize(len(records)).
-//	// 				Execute()
-//	// 		}
-//	// 		if err != nil {
-//	// 			return err
-//	// 		}
-//	// 		fmt.Println("Proccessed " + fmt.Sprint(i+1) + " records")
-//	// 		records = []model.Point{}
-//	// 	}
-//	// }
-//	// fmt.Println("Processing finished.")
+	// 	// construct data struct from point
+	// 	var newPoint model.Point
+	// 	for j, f := range fields {
+	// 		val := shpf.ReadAttribute(i, j)
+	// 		fieldStr := strings.Title(strings.ToLower(f.String()))
+	// 		structutil.SetField(&newPoint, fieldStr, val)
+	// 	}
+	// 	records = append(records, newPoint)
+	// 	// Batch upload on reaching batchsize limit
+	// 	if (i != 0 && i%batchSize == 0) || i == lastRecordIdx {
+	// 		if i == lastRecordIdx {
+	// 			// batching the last odd lot records doesn't work for some reason
+	// 			err = st.DS.Insert(&insertTable).
+	// 				Records(&records).
+	// 				Execute()
+	// 		} else {
+	// 			err = st.DS.Insert(&insertTable).
+	// 				Records(&records).
+	// 				Batch(true).
+	// 				BatchSize(len(records)).
+	// 				Execute()
+	// 		}
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		fmt.Println("Proccessed " + fmt.Sprint(i+1) + " records")
+	// 		records = []model.Point{}
+	// 	}
+	// }
+	// fmt.Println("Processing finished.")
 
-//	/////////////////////////////////////////////////////////
-//	// Fill out field + domain from included XLS
-//	file, err := excelize.OpenFile(cfg.FieldMap)
-//	if err != nil {
-//		return err
-//	}
-//	defer func() {
-//		// Close the spreadsheet.
-//		err = file.Close()
-//	}()
+	/////////////////////////////////////////////////////////
+	// Data insertion procedure:
+	//  Check if schema exists based on unique(name, version)
+	//      Yes -> reference id from existing schema
+	//      No -> insert new schema into store
+	//  Check if field exists based on unique(name, type)
+	//      Yes -> reference id for existing field
+	//      No ->
+	//          insert new field into store
+	//          Required domain?
+	//              Yes -> insert new domain referencing field
+	//              No -> continue
+	//  Check if field is associated to schema
+	//      Yes -> reference association
+	//      No -> add association
+	//  Check if dataset exists
+	//      Yes -> check if dataset has referenced schema
+	//          Yes -> reference id
+	//          No -> panic
+	//      No -> create new dataset
+	//  Insert inventory table using bash script
+	xlsF, err := xls.NewXls(cfg.XlsPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// Close the spreadsheet.
+		err = xlsF.Close()
+	}()
 
-//	//  Check if schema already exists
-//	//  If it is, then use id to populate dataset meta
-//	//  Otherwise, add a new schema
-//	var schemaId uuid.UUID
-//	schemaName, err := file.GetCellValue("schema", "A2")
-//	schemaVersion, err := file.GetCellValue("schema", "B2")
-//	schemaNotes, err := file.GetCellValue("schema", "C2")
+	/////////////////////////////////////////////////
+	//  SCHEMA
+	var schemaId uuid.UUID
+	schemaName, err := xlsF.GetCellValue("schema", "A2")
+	schemaVersion, err := xlsF.GetCellValue("schema", "B2")
+	schemaNotes, err := xlsF.GetCellValue("schema", "C2")
 
-//	schema := model.Schema{
-//		Name:    schemaName,
-//		Version: schemaVersion,
-//		Notes:   schemaNotes,
-//	}
-//	schemaId, err = st.GetSchemaId(schema)
-//	if err != nil {
-//		return err
-//	}
-//	if schemaId == uuid.Nil { // schema do not exists
-//		schemaId, err = st.AddSchema(schema)
+	schema := model.Schema{
+		Name:    schemaName,
+		Version: schemaVersion,
+		Notes:   schemaNotes,
+	}
+	schemaId, err = st.GetSchemaId(schema)
+	if err != nil {
+		return err
+	}
+	if schemaId == uuid.Nil {
+		schemaId, err = st.AddSchema(schema)
+	}
 
-//		// // Start adding in fields
-//		// rows, err := file.GetRows()
-//		// if err != nil {
-//		// 	fmt.Println(err)
-//		// 	return "", err
-//		// }
-//		// for _, row := range rows {
-//		// 	for _, colCell := range row {
-//		// 		fmt.Print(colCell, "\t")
-//		// 	}
-//		// 	fmt.Println()
-//		// }
+	// init data from shp file
+	fmt.Println("Reading shapefile from: " + cfg.ShpPath)
+	shpf, err := shp.Open(cfg.ShpPath)
+	if err != nil {
+		return err
+	}
+	defer shpf.Close()
 
-//		// Populate meta data
-//		fmt.Println("Reading shapefile from: " + cfg.FilePath)
-//		shpf, err := shp.Open(cfg.FilePath)
-//		if err != nil {
-//			return err
-//		}
-//		defer shpf.Close()
-//		shapeType := types.GeometryReverse[shpf.GeometryType]
+	fields := shpf.Fields()
+	for j, f := range fields {
 
-//		fields := shpf.Fields()
-//		for j, f := range fields {
-//			uuidNsiField := uuid.New()
-//			uuidDomain := uuid.New()
+		///////////////////////////////
+		//   FIELD
+		var fieldId uuid.UUID
+		fieldDescription, err := xlsF.GetCellValue("fields", "E"+fmt.Sprint(j+2))
+		if err != nil {
+			return err
+		}
+		sIsDomain, err := xlsF.GetCellValue("fields", "B"+fmt.Sprint(j+2))
+		if err != nil {
+			return err
+		}
+		bIsDomain, err := strconv.ParseBool(sIsDomain)
+		if err != nil {
+			return err
+		}
+		field := model.Field{
+			Name:        f.String(),
+			Type:        types.DatatypeReverse[string(f.Fieldtype)],
+			Description: fieldDescription,
+			IsDomain:    bIsDomain,
+		}
+		fieldId, err = st.GetFieldId(field)
+		if err != nil {
+			return err
+		}
+		if fieldId == uuid.Nil {
+			fieldId, err = st.AddField(field)
 
-//			// Field quantitative vs qualitative
-//			field := model.Field{
-//				Id:          uuidNsiField,
-//				Name:        f.String(),
-//				Type:        types.NsiFieldReverse[string(f.Fieldtype)],
-//				Description: "",
-//				IsDomain:    false,
-//			}
+			///////////////////////////////
+			//   SCHEMA_FIELD ASSOCIATION
+			flagAssociation, err := st.SchemaFieldAssociationExists(schemaId, fieldId)
+			if err != nil {
+				return err
+			}
+			if !flagAssociation {
+				_, err = st.AddSchemaFieldAssociation(schemaId, fieldId)
+				if err != nil {
+					return err
+				}
+			}
 
-//			domain := model.Domain{
-//				Id:         uuidDomain,
-//				NsiFieldId: uuidNsiField,
-//				Value:      0,
-//				Datatype:   "",
-//			}
-//		}
-//	}
+			///////////////////////////////
+			//   DOMAIN
+			// Process domain only if specified by field ie. field holds a discrete categorical variable
+			// Currently this is specified from the metadata xls, could be a TODO to automatically detect field based only on the shp file
+			if bIsDomain {
+				fieldVals := shape.UniqueValues(shpf, f)
+				if err != nil {
+					return err
+				}
+				for _, v := range fieldVals {
+					domain := model.Domain{
+						FieldId: fieldId,
+						Value:   v,
+					}
+					// This location can only be reached for new field inserts,
+					// can assume that domain do not exists
+					_, err := st.AddDomain(domain)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+	}
 
-//	// 	schemaField := model.SchemaField{
-//	// 		Id:         [16]byte{},
-//	// 		NsiFieldId: uuidNsiField,
-//	// 	}
+	////////////////////////////////////////
+	//  DATASET
 
-//	// 	schema := model.Schema{
-//	// 		Id:      [16]byte{},
-//	// 		Name:    schemaName,
-//	// 		Version: schemaVersion,
-//	// 		Notes:   schemaNotes,
-//	// 	}
-//	// 	err = st.AddSchema(schema)
+	datasetName, err := xlsF.GetCellValue("dataset", "C1")
+	datasetVersion, err := xlsF.GetCellValue("dataset", "C2")
+	datasetDescription, err := xlsF.GetCellValue("dataset", "C3")
+	datasetPurpose, err := xlsF.GetCellValue("dataset", "C4")
+	datasetCreatedBy, err := xlsF.GetCellValue("dataset", "C5")
+	sDatasetQuality, err := xlsF.GetCellValue("dataset", "C6")
+	if err != nil {
+		return err
+	}
 
-//	// 	dataset := model.Dataset{
-//	// 		Id:          [16]byte{},
-//	// 		Name:        "",
-//	// 		Version:     "",
-//	// 		NsiSchemaId: [16]byte{},
-//	// 		TableName:   "",
-//	// 		Shape:       types.Shape(shapeType),
-//	// 		Description: "",
-//	// 		Purpose:     "",
-//	// 		DateCreated: time.Time{},
-//	// 		CreatedBy:   "",
-//	// 		QualityId:   [16]byte{},
-//	// 	}
+	var qualityId uuid.UUID
+	quality := model.Quality{
+		Value: types.QualityReverse[sDatasetQuality],
+	}
+	qualityId, err = st.GetQualityId(quality)
+	if err != nil {
+		return err
+	}
+	if qualityId == uuid.Nil {
+		qualityId, err = st.AddQuality(quality)
+	}
 
-//	// }
+	dataset := model.Dataset{
+		Name:        datasetName,
+		Version:     datasetVersion,
+		SchemaId:    schemaId,
+		TableName:   "",
+		Shape:       types.ShapeReverse[shpf.GeometryType],
+		Description: datasetDescription,
+		Purpose:     datasetPurpose,
+		CreatedBy:   datasetCreatedBy,
+		QualityId:   qualityId,
+	}
 
-//	// // Upload data
-//	// cmd, err := exec.Command("/bin/bash", "./upload").Output()
+	_, err = st.AddDataset(dataset)
 
-//	// return string(cmd), err
-//	return err
-//}
+	// // Upload data
+	// _, err = exec.Command("/bin/bash", "./upload", "-d", "-s", "-c", "-t").Output()
+
+	// return string(cmd), err
+	return err
+}
+
+func ModAccess(cfg config.Config) error {
+	return nil
+}
+
+func AddQuality(cfg config.Config) error {
+	return nil
+}
