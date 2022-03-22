e527ca81507b296b13273dcc5dfbb770ee5d6d06
TMNT <trietpersonal@gmail.com>
2022-03-22 11:48:17 -0400

    scripts

---
+++ scripts/sql/quality.sql
@@ -0,0 +1,8 @@
+INSERT INTO quality (value, description)
+VALUES ('high', '');
+
+INSERT INTO quality (value, description)
+VALUES ('medium', '');
+
+INSERT INTO quality (value, description)
+VALUES ('low', '');
