5b80bcf675ce279b67ea03164247a4898e35787b
TMNT <trietpersonal@gmail.com>
2022-03-23 15:47:39 -0400

    regex :)

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,12 +2,12 @@ package config
 
 import (
 	"errors"
-	"regexp"
-	"strings"
+	"fmt"
 
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/model"
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/types"
 	"github.com/HydrologicEngineeringCenter/nsi-shape-loader/internal/util"
+	"github.com/dlclark/regexp2"
 	"github.com/google/uuid"
 	suuid "github.com/satori/go.uuid"
 	"github.com/urfave/cli/v2"
@@ -54,43 +54,84 @@ func NewConfig(c *cli.Context) (Config, error) {
 	mode := types.ModeReverse[c.String("mode")]
 	// validate for valid mode
 	if mode != types.Access && mode != types.Prep && mode != types.Upload {
-		return Config{}, errors.New("invalid mode, respecify --mode")
+		return Config{}, errors.New(fmt.Sprintf(
+			"invalid mode, --mode can only be %s, %s, or %s",
+			types.Access,
+			types.Prep,
+			types.Upload,
+		))
 	}
 
 	var storeCfg StoreConfig
 	var pathCfg PathConfig
 	var accessCfg model.Access
 
-	// validate nonempty db access info
-	if mode == types.Prep || mode == types.Upload {
-		// validate sql db connection is not empty
+	// validate sql connection creds
+	if mode == types.Access || mode == types.Upload {
 		sqlConn := c.String("sqlConn")
 		if sqlConn == "" {
 			return Config{}, errors.New("invalid sql connection string, --sqlConn should not be empty")
 		}
-		var user, pass, name, host, port string
-		user = strings.ReplaceAll(regexp.MustCompile(`user=.+?\s`).FindString(sqlConn), " ", "")
-		pass = strings.ReplaceAll(regexp.MustCompile(`password=.+?\s`).FindString(sqlConn), " ", "")
-		name = strings.ReplaceAll(regexp.MustCompile(`dbname=.+?\s`).FindString(sqlConn), " ", "")
-		host = strings.ReplaceAll(regexp.MustCompile(`host=.+?\s`).FindString(sqlConn), " ", "")
-		port = strings.ReplaceAll(regexp.MustCompile(`port=.+?\s`).FindString(sqlConn), " ", "")
+		var user, pass, database, host, port string
+		// std lib regex doesn't support lookahead and lookbehind????????????????????????????????????????????????????????????????????????????????????????????????????????
+		// user = strings.ReplaceAll(regexp2.MustCompile(`(?<=user=).+?(?=\s)`, 0).FindString(sqlConn), " ", "")
+		// pass = strings.ReplaceAll(regexp2.MustCompile(`(?<=password=).+?(?=\s)`, 0).FindString(sqlConn), " ", "")
+		// name = strings.ReplaceAll(regexp2.MustCompile(`(?<=name=).+?(?=\s)`).FindString(sqlConn), " ", "")
+		// host = strings.ReplaceAll(regexp2.MustCompile(`(?<=host=).+?(?=\s)`).FindString(sqlConn), " ", "")
+		// port = strings.ReplaceAll(regexp2.MustCompile(`(?<=port=).+?(?=\s)`).FindString(sqlConn), " ", "")
 
-		if util.StrContains([]string{sqlConn, user, pass, name, host, port}, "") {
+		var re *regexp2.Regexp
+		var m *regexp2.Match
+		var err error
+		re = regexp2.MustCompile(`(?<=user=).+?(?=\s|$)`, 0)
+		m, err = re.FindStringMatch(sqlConn)
+		if err != nil || m == nil {
+			return Config{}, errors.New("invalid sql connection string, unable to parse 'user' argument")
+		}
+		user = m.String()
+		re = regexp2.MustCompile(`(?<=password=).+?(?=\s|$)`, 0)
+		m, err = re.FindStringMatch(sqlConn)
+		if err != nil || m == nil {
+			return Config{}, errors.New("invalid sql connection string, unable to parse 'password' argument")
+		}
+		pass = m.String()
+		re = regexp2.MustCompile(`(?<=host=).+?(?=\s|$)`, 0)
+		m, err = re.FindStringMatch(sqlConn)
+		if err != nil || m == nil {
+			return Config{}, errors.New("invalid sql connection string, unable to parse 'host' argument")
+		}
+		host = m.String()
+		re = regexp2.MustCompile(`(?<=port=).+?(?=\s|$)`, 0)
+		m, err = re.FindStringMatch(sqlConn)
+		if err != nil || m == nil {
+			return Config{}, errors.New("invalid sql connection string, unable to parse 'port' argument")
+		}
+		port = m.String()
+		re = regexp2.MustCompile(`(?<=database=).+?(?=\s|$)`, 0)
+		m, err = re.FindStringMatch(sqlConn)
+		if err != nil || m == nil {
+			return Config{}, errors.New("invalid sql connection string, unable to parse 'database' argument")
+		}
+		database = m.String()
+
+		if util.StrContains([]string{sqlConn, user, pass, database, host, port}, "") {
 			return Config{}, errors.New("invalid sql connection string, respecify --sqlConn")
 		}
-
 		storeCfg = StoreConfig{
 			Dbuser: user,
 			Dbpass: pass,
-			Dbname: name,
+			Dbname: database,
 			Dbhost: host,
 			Dbport: port,
 		}
+	}
+
+	// validate file pathings
+	if mode == types.Prep || mode == types.Upload {
 		pathCfg = PathConfig{
 			ShpPath: c.Path("shpPath"),
 			XlsPath: c.Path("xlsPath"),
 		}
-
 		if pathCfg.ShpPath == "" {
 			return Config{}, errors.New("invalid path to shp file, --shpPath should not be empty")
 		}
@@ -99,10 +140,16 @@ func NewConfig(c *cli.Context) (Config, error) {
 		}
 	}
 
+	// validate access mod params
 	if mode == types.Access {
 		role := types.Role(c.String("role"))
 		if !util.StrContains([]string{string(types.Admin), string(types.Owner), string(types.User)}, string(role)) {
-			return Config{}, errors.New("invalid role, --role accepts only admin, owner, or user")
+			return Config{}, errors.New(fmt.Sprintf(
+				"invalid role, --role accepts only %s, %s, or %s",
+				types.Admin,
+				types.Owner,
+				types.User,
+			))
 		}
 		group := c.String("group")
 		if group == "" {
